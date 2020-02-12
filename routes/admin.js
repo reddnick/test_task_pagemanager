f1c28a5d0ba431d2619e314be51f83f9019a69d4
Oleg <oleh.cherednik@gmail.com>
2020-02-12 14:05:38 +0200

    Fix login, register, slug errors,

--- routes/admin.js
+++ routes/admin.js
@@ -8,18 +8,17 @@ router.get('/pages', auth.jwt, function (req, res) {
         .then((pages) => res.render('admin/page/list.hbs', {pages}))
 });
 
-
 router.get('/pages/create', auth.jwt, function (req, res) {
     res.render('admin/page/create.hbs')
 });
 
 router.post('/pages/create', auth.jwt, async function (req, res) {
     const page = await PageModel.findOne({where: {slug: req.body.slug}});
-    if(!page) {
-       PageModel.create(req.body).then(() => res.redirect('/admin/pages'))
+    if (!page) {
+        PageModel.create(req.body).then(() => res.redirect('/admin/pages'))
     } else {
         let currPage = req.body;
-        res.render('admin/page/create',   {currPage, error: 'Slug already exist(must be unique)'})
+        res.render('admin/page/create', {currPage, error: 'Slug already exist(must be unique)'})
     }
 });
 
@@ -33,18 +32,16 @@ router.get('/pages/:id/edit', auth.jwt, function (req, res) {
 router.post('/pages/:id/edit', auth.jwt, async function (req, res) {
     const existSlug = await PageModel.findOne({where: {slug: req.body.slug}});
     const currentSlug = await PageModel.findOne({where: {id: req.params.id}});
-    if(!existSlug || existSlug.slug === currentSlug.slug) {
+    if (!existSlug || existSlug.slug === currentSlug.slug) {
         PageModel.update(req.body, {
             where: {id: req.params.id}
         })
             .then(() => res.redirect('/admin/pages'));
-    }
-    else {
+    } else {
         let page = req.body;
-        res.render('admin/page/edit',   {page, error: 'Slug already exist(must be unique)'})
-        }
+        res.render('admin/page/edit', {page, error: 'Slug already exist(must be unique)'})
+    }
 });
-
 
 
 router.post('/pages/:id/delete', auth.jwt, function (req, res) {
